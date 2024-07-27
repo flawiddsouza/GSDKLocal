3cf6ebf8e2dc01b67c101432c7effcaf22a00e2c
Flawid DSouza <me@flawiddsouza.com>
2024-07-27 18:32:07 +0530

    to prevent concurrency issues while getting a port, we will process requestMultiplayerServer requests sequentially using a mutex lock

--- app.ts
+++ app.ts
@@ -18,6 +18,7 @@ import { Constants } from './constants';
 import { fileURLToPath } from 'url';
 import * as db from './db';
 import { GameServerInstance } from './schema';
+import { Mutex } from 'async-mutex';
 import dotenv from 'dotenv'
 
 dotenv.config()
@@ -55,91 +56,105 @@ if (!process.env.PUBLIC_IP) {
 
 console.log('Public IP:', process.env.PUBLIC_IP);
 
+// Create the mutex
+const portMutex = new Mutex();
+
 fastify.post('/requestMultiplayerServer', async (request, reply): Promise<PlayFabRequestMultiplayer.Response | SafeParseValidationErrorResponse<RequestMultiplayerServerRequestBody> | ValidationErrorResponse> => {
-  const validationResult = requestMultiplayerServerRequestBodySchema.safeParse(request.body);
+  // Wait for the lock to be available
+  const release = await portMutex.acquire();
 
-  if (!validationResult.success) {
-    reply.type('application/json').code(400)
-    return { error: validationResult }
-  }
+  try {
+    const validationResult = requestMultiplayerServerRequestBodySchema.safeParse(request.body);
 
-  const body: RequestMultiplayerServerRequestBody = validationResult.data;
-
-  const build = await db.getBuild(body.BuildId);
-
-  if (!build) {
-    reply.type('application/json').code(400)
-    return { error: 'Build not found' }
-  }
-
-  const port = await db.getPort();
-
-  if (!port) {
-    reply.type('application/json').code(400)
-    return { error: 'No ports available' }
-  }
-
-  const gameServerInstance: GameServerInstance = {
-    serverId: '',
-    buildId: body.BuildId,
-    sessionConfig: {
-      sessionId: body.SessionId,
-      sessionCookie: body.SessionCookie,
-      metadata: { gamePort: port }
-    },
-    port: port,
-  };
-
-  const docker = new Docker();
-
-  const gsdkConfigPath = path.join(dockerDataDirectory, Constants.GSDK_CONFIG_FILENAME);
-
-  const container = await docker.createContainer({
-    Image: build.imageName,
-    HostConfig: {
-      NetworkMode: 'host',
-      PortBindings: {
-        [`${port}/tcp`]: [{ HostPort: port }]
-      },
-      Mounts: [
-        {
-          Target: '/data',
-          Source: dockerDataDirectory,
-          Type: 'bind',
-        },
-      ],
-    },
-    Env: [
-      `GSDK_CONFIG_FILE=/data/${Constants.GSDK_CONFIG_FILENAME}`,
-    ],
-  });
-
-  gameServerInstance.serverId = container.id;
-
-  const publicIp = process.env.PUBLIC_IP;
-
-  writeFileSync(gsdkConfigPath, JSON.stringify({
-    heartbeatEndpoint: `${publicIp}:9006`,
-    sessionHostId: gameServerInstance.serverId,
-    logFolder: `/data/GameLogs/${gameServerInstance.serverId}/`,
-  }, null, 4));
-
-  await db.createGameServerInstance(gameServerInstance);
-
-  await container.start();
-
-  reply.type('application/json').code(200)
-  return {
-    code: 200,
-    data: {
-      ServerId: gameServerInstance.serverId,
-      IPV4Address: publicIp,
-      Ports: [{
-        Num: Number(port),
-      }],
-      LastStateTransitionTime: new Date(),
+    if (!validationResult.success) {
+      reply.type('application/json').code(400)
+      return { error: validationResult }
     }
-  };
+
+    const body: RequestMultiplayerServerRequestBody = validationResult.data;
+
+    const build = await db.getBuild(body.BuildId);
+
+    if (!build) {
+      reply.type('application/json').code(400)
+      return { error: 'Build not found' }
+    }
+
+    const port = await db.getPort();
+
+    if (!port) {
+      reply.type('application/json').code(400)
+      return { error: 'No ports available' }
+    }
+
+    const gameServerInstance: GameServerInstance = {
+      serverId: '',
+      buildId: body.BuildId,
+      sessionConfig: {
+        sessionId: body.SessionId,
+        sessionCookie: body.SessionCookie,
+        metadata: { gamePort: port }
+      },
+      port: port,
+    };
+
+    const docker = new Docker();
+
+    const gsdkConfigPath = path.join(dockerDataDirectory, Constants.GSDK_CONFIG_FILENAME);
+
+    const container = await docker.createContainer({
+      Image: build.imageName,
+      HostConfig: {
+        NetworkMode: 'host',
+        PortBindings: {
+          [`${port}/tcp`]: [{ HostPort: port }]
+        },
+        Mounts: [
+          {
+            Target: '/data',
+            Source: dockerDataDirectory,
+            Type: 'bind',
+          },
+        ],
+      },
+      Env: [
+        `GSDK_CONFIG_FILE=/data/${Constants.GSDK_CONFIG_FILENAME}`,
+      ],
+    });
+
+    gameServerInstance.serverId = container.id;
+
+    const publicIp = process.env.PUBLIC_IP;
+
+    writeFileSync(gsdkConfigPath, JSON.stringify({
+      heartbeatEndpoint: `${publicIp}:9006`,
+      sessionHostId: gameServerInstance.serverId,
+      logFolder: `/data/GameLogs/${gameServerInstance.serverId}/`,
+    }, null, 4));
+
+    await db.createGameServerInstance(gameServerInstance);
+
+    await container.start();
+
+    reply.type('application/json').code(200)
+    return {
+      code: 200,
+      data: {
+        ServerId: gameServerInstance.serverId,
+        IPV4Address: publicIp,
+        Ports: [{
+          Num: Number(port),
+        }],
+        LastStateTransitionTime: new Date(),
+      }
+    };
+  } catch (err) {
+    reply.type('application/json').code(500);
+    return { error: 'Internal server error' };
+  } finally {
+    // Release the lock
+    release();
+  }
 });
 
 fastify.patch('/v1/sessionHosts/:serverId', async (request, reply) => {
